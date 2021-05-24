f861def5002dcf41691218d140e78692abbc1cb9
duncanjake1 <jacobsenduncan@gmail.com>
2021-05-24 14:18:01 -0600

    added content type to views.py

--- podcast_data/views.py
+++ podcast_data/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
 from rest_framework import generics
-
 from .models import Avalanche_Accident
 from .serializers import Avalanche_AccidentSerializer
+from django.views import View
+from django.http import HttpResponse, HttpResponseNotFound
+import os
 
 class ListAvalanche_Accident(generics.ListCreateAPIView):
     queryset = Avalanche_Accident.objects.all()
@@ -11,4 +12,15 @@ class ListAvalanche_Accident(generics.ListCreateAPIView):
 
 class DetailAvalanche_Accident(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avalanche_Accident.objects.all()
-    
+
+# load static files (frontend react app)
+class Assets(View):
+
+    def get(self, _request, filename):
+        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                return HttpResponse(file.read(), content_type='application/javascript')
+        else:
+            return HttpResponseNotFound()
