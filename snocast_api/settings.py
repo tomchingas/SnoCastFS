3f8e894ba1a94fcab841d256790d7c101c92be98
duncanjake1 <jacobsenduncan@gmail.com>
2021-05-24 22:43:14 -0600

    additional configuration

--- snocast_api/settings.py
+++ snocast_api/settings.py
@@ -148,7 +148,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, 'build', 'static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
